711e40acf83eb31d580dc99129edb22601bba583
Rob Ashton <robashton@codeofrob.com>
2013-08-24 12:32:07 +0100

    Bounds checking

--- game/physics.js
+++ game/physics.js
@@ -3,5 +3,15 @@ var apply = exports.apply = function(rect) {
   rect.y += rect.vy
   rect.vx *= (1.0 - rect.friction)
   rect.vy *= (1.0 - rect.friction)
+  return restrictBoundsOf(rect)
+}
+
+function restrictBoundsOf(rect) {
+  if((rect.x < 0 && rect.vx < 0) ||
+    (rect.x > 640 && rect.vx > 0))
+      rect.vx = -rect.vx
+  if((rect.y < 0 && rect.vy < 0) ||
+    (rect.y > 480 && rect.vy > 0))
+      rect.vy = -rect.vy
   return rect
 }
